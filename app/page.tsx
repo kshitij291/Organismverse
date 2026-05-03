1cea3eaca097c38c560cc0dc7b1de8204cf6763c
kshitij291 <kshitijsingh0109@gmail.com>
2026-05-03 11:14:49 +0530

    Add Home component with title and description

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🌍 OrganismVerse</h1>
+      <p>Search animals and learn about nature</p>
+    </div>
+  )
+}

b6341920e950193d05a4474045222ecd5dd4fbb0
kshitij291 <kshitijsingh0109@gmail.com>
2026-05-03 11:23:35 +0530

    Create supabase.ts

---
+++ app/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://yluhahlkwxbulprlckds.supabase.co",
+  "sb_publishable_pQN48Xno7iGIUjP4afYcCw_SmvAAvsW"
+)
